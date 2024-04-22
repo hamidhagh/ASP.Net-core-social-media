f14b362cee46a012665cd84f930b05e789ac2f5d
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-04-22 08:47:02 -0700

    Paging, filtering,sorting and last active action filter implemented

--- SocialMedia/API/Controllers/BaseApiController.cs
+++ SocialMedia/API/Controllers/BaseApiController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
